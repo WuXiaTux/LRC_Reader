72c44ac338d9b3bb6e293ef51c987162dbbd02c9
WuXiaTux <1703059013@qq.com>
2025-03-28 00:05:46 +0800

    v2.0
    v2.0更新

---
+++ v2_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY Red Panda C++ */
+/* DO NOT EDIT ! */
+
+#ifndef v2_private_rc
+#define v2_private_rc
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.0.0.0"
+#define VER_MAJOR	2
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"武侠Tux"
+#define FILE_VERSION	"2.0.0.0"
+#define FILE_DESCRIPTION	"让音乐播放器支持观看TXT文件的工具"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright 2024 武侠Tux. All rights reserved."
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"LRC阅读器"
+#define PRODUCT_NAME	"LRC阅读器"
+#define PRODUCT_VERSION	"2.0.0.0"
+
+#endif /*v2_private_rc*/
